f435f8935f15e90ee17d3f11219781e72a73e139
Brad West <bradwest2@gmail.com>
2017-07-02 20:02:18 -0700

    Add Decision Tree for email prediction, use min_samples_split=40, L4 Quiz37

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,10 +20,23 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from time import time
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+print 'The number of features in the training data is', len(features_train[0])
+
+t = time()
+clf.fit(features_train,labels_train)
+print 'The tree training time is ', round(time()-t,3), 'seconds' 
+
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test,pred)
+print 'The Tree Accuracy  with min_sample_split=40 is ', round(acc*100,3), '%'
 
 
 #########################################################
