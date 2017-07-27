85da6b557c4facd557b8cd68089c346532fc98c0
Brad West <bradwest2@gmail.com>
2017-07-27 01:57:51 -0700

    Lesson 8 Mini-Project up to part 14

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -2,19 +2,22 @@
 
 
 def outlierCleaner(predictions, ages, net_worths):
-    """
-        Clean away the 10% of points that have the largest
-        residual errors (difference between the prediction
-        and the actual net worth).
+	"""
+		Clean away the 10% of points that have the largest
+		residual errors (difference between the prediction
+		and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
-        each tuple is of the form (age, net_worth, error).
-    """
-    
-    cleaned_data = []
+		Return a list of tuples named cleaned_data where 
+		each tuple is of the form (age, net_worth, error).
+	"""
 
-    ### your code goes here
+	cleaned_data = []
 
-    
-    return cleaned_data
+	### your code goes here
+	residuals = (predictions-net_worths)**2
+	pts2rmv = int(len(predictions)*0.9)
+	cleaned_data = sorted(zip(ages,net_worths, residuals),key=lambda err: err[2])[:pts2rmv]
+	
+	
+	return cleaned_data
 
