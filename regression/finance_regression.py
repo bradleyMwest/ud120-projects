5763d4bfefd7c7e842eb8ad9385b28b05190e311
Brad West <bradwest2@gmail.com>
2017-07-26 20:15:07 -0700

    Finished Lesson 7

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -39,10 +39,15 @@ test_color = "b"
 ### "r" to differentiate training points from test points.
 
 
+from sklearn.linear_model import LinearRegression
+ 
+reg = LinearRegression()
 
-
-
-
+reg.fit(feature_train, target_train)
+print 'The slope is ', reg.coef_ 
+print 'The intercept is ', reg.intercept_
+print 'The R2 on the training data is', reg.score(feature_train, target_train)
+print 'The R2 on the test data is', reg.score(feature_test,target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -67,4 +72,5 @@ except NameError:
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
+plt.tight_layout()
 plt.show()
